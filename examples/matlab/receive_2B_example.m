e24dc460601885214b76617a86ada52870839e8d
DawidK1 <dawidk421@gmail.com>
2017-05-08 13:16:31 +0200

    Create receive_2B_example.m

---
+++ examples/matlab/receive_2B_example.m
@@ -0,0 +1,37 @@
+%open com port, speed 115200, one stop bit, no parity bits
+s = serial('COM11');
+set(s,'BaudRate',115200);
+s.StopBits=1;
+s.Parity='none';
+  fopen(s);
+s
+
+while 1
+    msg= fread(s);
+    first_list=find(msg == 4);
+
+    for iter = 1:length(first_list)
+        first = first_list(iter);
+        
+        
+        if  (first+5 < length(msg)) &&...   
+            (msg(first+5) < 9 ) &&... 
+            (first +6 + msg(first+5) + 1 < length(msg))&&...
+            (msg(first + 5 + msg(first+5) + 1 ) == 170)
+           
+            parse =msg(first:(6 + first + msg(first+5)));
+            ID = bitshift(parse(5),21)+ bitshift(parse(4),13) + bitshift(parse(3),5) + parse(2);
+            dec2hex(ID);
+            DLC = parse(6);
+            output = dec2hex(ID);
+    
+                output = strcat('ID=',output, ' DLC = ', num2str(DLC), ' DATA = ');
+                for i =1:DLC
+                     output = strcat(output, num2str(parse(6 +i)), '. '); % glue together frame
+                end
+                disp(output) % show in command window
+                
+        end      
+    end
+
+end
