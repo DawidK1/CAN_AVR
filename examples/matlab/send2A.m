ffddc94a5f67d58b2ee3eb10c55db04d5ce226a1
DawidK1 <dawidk421@gmail.com>
2017-05-08 13:04:28 +0200

    Create send2A.m

---
+++ examples/matlab/send2A.m
@@ -0,0 +1,16 @@
+function send2A(s,ID,DLC,DATA)%this function is used to send 2.0A standard CAN frame to CAN-AVR
+
+    CMD=1; %command to send not-extended frame
+
+    ID = uint16(ID);
+    
+    IDtag0=uint8(mod(ID,256));
+    IDtag1=uint8(mod(shiftbit(ID,-8),256) );
+
+
+    buff=uint8([CMD, IDtag0, IDtag1, DLC, DATA]);
+
+    fwrite(s,buff);
+
+
+end
